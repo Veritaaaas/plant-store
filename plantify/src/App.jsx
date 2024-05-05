5c9340e7ab9cf6999cd5dafd3bb6d6e5fe326574
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-05 15:07:15 +0800

    chore: Update npm dependencies for FontAwesome to version 6.5.2

--- plantify/src/App.jsx
+++ plantify/src/App.jsx
@@ -1,11 +1,11 @@
-
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <Navbar />
     </>
   )
 }
--- plantify/src/components/Navbar.jsx
+++ plantify/src/components/Navbar.jsx
@@ -1,9 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
+
 
 function Navbar() {
   return (
 
-    <nav data-testid="navbar" className="navbar navbar-expand-lg navbar-light bg-light">
-        <a href="">Home</a>
+    <nav data-testid="navbar" className="flex pl-20 pr-20 pt-8">
+        <div>
+            <h2>Plantify</h2>
+        </div>
+        <div>
+            <a href="">Home</a>
+            <a href="">Products</a>
+            <a href="">About</a>
+        </div>
+        <div>
+            <FontAwesomeIcon icon={faShoppingCart} data-testid="shopping-cart-icon" />
+            <FontAwesomeIcon icon={faUser} data-testid="profile" />
+        </div>
     </nav>
   )
 }
