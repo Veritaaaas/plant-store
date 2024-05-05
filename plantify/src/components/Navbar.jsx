d73a3a3a2c5bb72d980f7d4cdcacee0d5261479c
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-05 15:25:28 +0800

    chore: Update Navbar styling and structure

--- plantify/src/components/Navbar.jsx
+++ plantify/src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
-
+import './Navbar.css';
 
 function Navbar() {
   return (
 
-    <nav data-testid="navbar" className="flex pl-20 pr-20 pt-8">
-        <div>
-            <h2>Plantify</h2>
+    <nav data-testid="navbar" className="grid custom-grid pl-20 pr-20 pt-8 pb-4
+                                        content-center">
+        <div className='flex content-center text-4xl custom-font'>
+            <h2 className='m-0 p-0'>Plantify</h2>
         </div>
-        <div>
+        <div className='flex justify-start gap-20 ml-20 content-center text-m'>
             <a href="">Home</a>
             <a href="">Products</a>
             <a href="">About</a>
         </div>
-        <div>
+        <div className='flex justify-between text-lg'>
             <FontAwesomeIcon icon={faShoppingCart} data-testid="shopping-cart-icon" />
             <FontAwesomeIcon icon={faUser} data-testid="profile" />
         </div>
