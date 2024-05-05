b190da04189384ee777f1d6bfc1a467a611f138a
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-05 15:06:35 +0800

    added test for navbar elements

--- plantify/src/tests/Navbar.test.jsx
+++ plantify/src/tests/Navbar.test.jsx
@@ -9,4 +9,21 @@ describe("Navbar", () => {
         const navbarElement = screen.getByTestId("navbar");
         expect(navbarElement).toBeInTheDocument();
     });
+
+    it("renders all links", () => {
+        render(<Navbar />);
+        const logoElement = screen.getByText("Plantify");
+        const homeLink = screen.getByText("Home");
+        const productsLink = screen.getByText("Products");
+        const aboutLink = screen.getByText("About");
+        const shoppingCartIcon = screen.getByTestId("shopping-cart-icon");
+        const profile = screen.getByTestId("profile");
+
+        expect(logoElement).toBeInTheDocument();
+        expect(homeLink).toBeInTheDocument();
+        expect(productsLink).toBeInTheDocument();
+        expect(aboutLink).toBeInTheDocument();
+        expect(shoppingCartIcon).toBeInTheDocument();
+        expect(profile).toBeInTheDocument();
+    });
 })
