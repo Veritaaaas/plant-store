475afdeaa4bea39639d4275ab59682a9d567a095
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-05 14:45:39 +0800

    set up the testing environment

--- plantify/src/tests/Navbar.test.jsx
+++ plantify/src/tests/Navbar.test.jsx
@@ -1,6 +1,5 @@
 //importing the necessary modules
 import {render, screen} from '@testing-library/react';
-import {describe, it, expect} from '@jest/globals'; 
 import Navbar from '../components/Navbar';
 
 //test to check if the Navbar component is rendered
