46f0eef641b740ae4d9b1f8fdd86b69cbfa40bf6
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-09 22:27:07 +0800

    Update Navbar styling and structure

--- plantify/src/components/Navbar.jsx
+++ plantify/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 
 function Navbar() {
   return (
-    <header className="flex gap-5 justify-between items-start px-0.5 w-full max-md:flex-wrap max-md:max-w-full pt-4" data-testid="navbar">
-      <div className="flex gap-5 mt-1.5 whitespace-nowrap max-md:flex-wrap max-md:max-w-full">
-        <h1 className="flex-auto text-3xl text-black">Plantify</h1>
+    <header className="flex gap-5 justify-between items-start px-0.5 w-full pt-4 border-b-2 border-black" data-testid="navbar">
+      <div className="flex gap-5 mt-1.5 whitespace-nowrap">
+        <h1 className="flex-auto text-2xl text-black">Plantify</h1>
         <nav className="flex flex-auto ml-20 gap-10 justify-between self-start text-lg font-medium text-stone-900 text-opacity-50">
           <a href="#">Home</a>
           <a href="#">Products</a>
@@ -11,8 +11,9 @@ function Navbar() {
         </nav>
       </div>
       <div className="flex gap-5 justify-between">
-        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/1e2f479a15be02d05dc0b55f501a147fb34b583f9ff035e94ef773eb2922f563?apiKey=8b5c844c9e604efba227618c0cf9b444&" alt="" className="shrink-0 self-start aspect-square w-[35px]" data-testid="shopping-cart-icon"/>
-        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/70e88fdb447737848c1163d274879c36c7deb93064ab54c71af5932cfb635418?apiKey=8b5c844c9e604efba227618c0cf9b444&" alt="" className="shrink-0 w-9 aspect-square" data-testid="profile"/>
+        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/1e2f479a15be02d05dc0b55f501a147fb34b583f9ff035e94ef773eb2922f563?apiKey=8b5c844c9e604efba227618c0cf9b444&" alt="" className="shrink-0 w-6 aspect-square" data-testid="shopping-cart-icon"/>
+        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/f69d4ffe1982e44a988d673c998cb49f8a46ea6c9d9bd2f57d76ff719699a08c?apiKey=8b5c844c9e604efba227618c0cf9b444&" alt="" className="shrink-0 w-5 aspect-square" />
+        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/97e04eece18ef753b843c79800bfca7ebd4a798b8b9c4f596fe361bf1a14ff56?apiKey=8b5c844c9e604efba227618c0cf9b444&" alt="" className="shrink-0 w-6 aspect-square" />
       </div>
     </header>
   )
