63702db8d2e6410ead2462cd6f85e7e9ea789bb3
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-11 23:00:11 +0800

    feat: Add Home component for the landing page

---
+++ plantify/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "./Banner";
+import BestSellers from "./Best_sellers";
+
+function Home() {
+    return (
+        <div>
+            <Banner />
+            <BestSellers />
+        </div>
+    );
+}
+
+export default Home;
