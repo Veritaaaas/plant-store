9b067d79be4b58b778ad5ddd7eacdfb09dcb1ef2
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-11 23:02:10 +0800

    added react-router-dom

--- plantify/tailwind.config.js
+++ plantify/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-1": "var(--color-1)",
+        "color-2": "var(--color-2)",
+        "color-3": "var(--color-3)",
+        "color-4": "var(--color-4)",
+      },
+    },
   },
   plugins: [],
 }
