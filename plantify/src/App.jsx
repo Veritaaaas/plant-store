72a03517db305cfd0b062478949ee94fa602d6df
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-11 22:59:35 +0800

    chore: Update App.jsx to include Home component

--- plantify/src/App.jsx
+++ plantify/src/App.jsx
@@ -1,13 +1,17 @@
-import Navbar from './components/Navbar'
-import './App.css'
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import './App.css';
+
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
