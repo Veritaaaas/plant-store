0fadc1c7878ea8fca7ba6f1b887e441ece17a460
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-11 23:01:43 +0800

    feat: Add Shop component for displaying the shop page and routing for choosing product display

--- plantify/src/components/Shop_Header.jsx
+++ plantify/src/components/Shop_Header.jsx
@@ -1,22 +1,22 @@
+import { NavLink } from 'react-router-dom';
 
-function Shop_Header( {product_display, setProductDisplay} ) {
-
+function Shop_Header() {
   return (
     <>
-        <h1 className="flex justify-center mt-4 text-4xl font-extrabold text-black leading-[64px]">
-          Plant Marketplace
-        </h1>
-        <div className="flex gap-4 justify-center mt-4 text-base font-extrabold leading-6 text-black">
-          <button className={`justify-center px-5 py-2.5 text-sm border-2 border-black border-solid ${product_display === 'natural' ? 'text-white bg-black' : 'bg-white'}`} onClick={() => setProductDisplay("natural")}>
-            Natural
-          </button>
-          <button className={`justify-center px-5 py-2.5 text-sm border-2 border-black border-solid ${product_display === 'bonsai' ? 'text-white bg-black' : 'bg-white'}`} onClick={() => setProductDisplay("bonsai")}>
-            Bonsai
-          </button>
-          <button className={`justify-center px-5 py-2.5 text-sm border-2 border-black border-solid ${product_display === 'flower' ? 'text-white bg-black' : 'bg-white'}`} onClick={() => setProductDisplay("flower")}>
-            Flower
-          </button>
-        </div>
+      <h1 className="flex justify-center mt-4 text-4xl font-extrabold text-black leading-[64px]">
+        Plant Marketplace
+      </h1>
+      <div className="flex gap-4 justify-center mt-4 text-base font-extrabold leading-6 text-black">
+        <NavLink to="/shop/natural" activeClassName="active" className='justify-center px-5 py-2.5 text-sm border-2 border-black border-solid'>
+          Natural
+        </NavLink>
+        <NavLink to="/shop/bonsai" activeClassName="active" className='justify-center px-5 py-2.5 text-sm border-2 border-black border-solid'>
+          Bonsai
+        </NavLink>
+        <NavLink to="/shop/flower" activeClassName="active" className='justify-center px-5 py-2.5 text-sm border-2 border-black border-solid'>
+          Flower
+        </NavLink>
+      </div>
     </>
   );
 }
