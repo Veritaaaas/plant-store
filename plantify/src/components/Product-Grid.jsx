c01f76031607f1c8a9b3247db3e1e6b06ce8e185
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-12 23:30:53 +0800

    refactor: Update Product_Grid component to include product_display prop

--- plantify/src/components/Product-Grid.jsx
+++ plantify/src/components/Product-Grid.jsx
@@ -1,13 +1,13 @@
 import ProductCard from "./Product_card";
 
 
-function Product_Grid({ products }) {
+function Product_Grid({ products, product_display }) {
   return (
     <div className="flex justify-center">
         <div className="grid grid-cols-3 gap-32 mt-12 mb-8">
         {products.map((product, index) => (
                 <div key={index} className="flex-col ml-5 w-[280px]">
-                    <ProductCard {...product} />
+                    <ProductCard image={product.image} name={product.name} price={product.price} product_display={product_display} />
                 </div>
             ))}
         </div>
