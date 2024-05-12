aa88b2ca700a359f5470a11896465d286a2e5157
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-12 23:30:47 +0800

    feat: Add react-router-dom and update Product_card component for routing to product details

--- plantify/src/components/Product_card.jsx
+++ plantify/src/components/Product_card.jsx
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-function ProductCard({ image, name, price }) {
+function ProductCard({ image, name, price, product_display }) {
     return (
-        <div className="flex flex-col grow text-lg font-medium max-md:mt-9">
-        <img loading="lazy" src={image} alt={name} className="w-full aspect-[0.83]" />
-        <div className="mt-8 text-stone-900">{name}</div>
-        <div className="mt-2 text-stone-900 text-opacity-50">{price}</div>
-        </div>
+        <Link to={`/shop/${product_display}/${name}`}>
+            <div className="flex flex-col grow text-lg font-medium max-md:mt-9">
+            <img src={image} alt={name} className="w-full aspect-[0.83]" />
+            <div className="mt-8 text-stone-900">{name}</div>
+            <div className="mt-2 text-stone-900 text-opacity-50">{price}</div>
+            </div>
+        </Link>
     );
 }
 
