64ba868f3318175630bb65f6c165831601fb612d
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-13 19:01:14 +0800

    refactor: Update Shop_Header component to use react-router-dom for navigation

--- plantify/src/components/Shop_Header.jsx
+++ plantify/src/components/Shop_Header.jsx
@@ -1,21 +1,33 @@
-import { NavLink } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Shop_Header() {
+  const location = useLocation();
+
   return (
     <>
       <h1 className="flex justify-center mt-4 text-4xl font-extrabold text-black leading-[64px]">
         Plant Marketplace
       </h1>
       <div className="flex gap-4 justify-center mt-4 text-base font-extrabold leading-6 text-black">
-        <NavLink to="/shop/natural" activeClassName="active" className='justify-center px-5 py-2.5 text-sm border-2 border-black border-solid'>
+        <Link 
+          to="/shop/natural" 
+          className={`justify-center px-5 py-2.5 text-sm border-2 border-black border-solid ${location.pathname === '/shop/natural' ? 'active_category' : ''}`}
+        >
           Natural
-        </NavLink>
-        <NavLink to="/shop/bonsai" activeClassName="active" className='justify-center px-5 py-2.5 text-sm border-2 border-black border-solid'>
+        </Link>
+        <Link 
+          to="/shop/bonsai" 
+          className={`justify-center px-5 py-2.5 text-sm border-2 border-black border-solid ${location.pathname === '/shop/bonsai' ? 'active_category' : ''}`}
+        >
           Bonsai
-        </NavLink>
-        <NavLink to="/shop/flower" activeClassName="active" className='justify-center px-5 py-2.5 text-sm border-2 border-black border-solid'>
+        </Link>
+        <Link 
+          to="/shop/flower" 
+          className={`justify-center px-5 py-2.5 text-sm border-2 border-black border-solid ${location.pathname === '/shop/flower' ? 'active_category' : ''}`}
+        >
           Flower
-        </NavLink>
+        </Link>
       </div>
     </>
   );
