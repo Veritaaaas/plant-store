91374164f1e83663674ca90c9eb9f8597ed53f6c
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-13 19:01:07 +0800

    refactor: Remove Navbar component from Shop.jsx

--- plantify/src/components/Shop.jsx
+++ plantify/src/components/Shop.jsx
@@ -1,6 +1,5 @@
 import { useParams } from 'react-router-dom';
 
-import Navbar from './Navbar';
 import Shop_Header from './Shop_Header';
 import Product_Grid from './Product-Grid';
 import product_data from '../assets/product_data';
@@ -13,7 +12,6 @@ function Shop() {
 
   return (
     <>
-        <Navbar />
         <Shop_Header />
         <Product_Grid products={product_display === 'natural' ? natural_products : product_display === 'bonsai' ? 
         bonsai_products : flower_products} product_display={product_display}/>
