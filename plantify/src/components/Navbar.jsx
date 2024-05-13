1a64116dd3fd97b39a738555b7306df16da0c7ee
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-13 21:55:39 +0800

    refactor: Update Navbar component to include search functionality

--- plantify/src/components/Navbar.jsx
+++ plantify/src/components/Navbar.jsx
@@ -5,12 +5,13 @@ import { FaBars } from 'react-icons/fa';
 function Navbar() {
   const [activeLink, setActiveLink] = useState('/');
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   return (
     <header className="flex flex-col lg:flex-row gap-5 justify-between items-start px-0.5 w-full pt-4 border-b-2 border-black" data-testid="navbar">
       <div className="flex gap-5 mt-1.5 whitespace-nowrap max-md:flex max-md:justify-between max-md:w-full">
         <Link to="/" onClick={() => setActiveLink('/')}>
-          <h1 className="flex-auto text-2xl text-black">Plantify</h1>
+          <h1 className="flex-auto text-2xl text-black Poetsin">Plantify</h1>
         </Link>
         <nav className="hidden lg:flex flex-auto ml-20 gap-10 justify-between self-start text-lg font-medium text-stone-900 text-opacity-50">
           <NavLink to="/" className={activeLink === '/' ? 'active' : ''} onClick={() => setActiveLink('/')}>Home</NavLink>
@@ -31,11 +32,18 @@ function Navbar() {
           </div>
       </nav>}
       <div className="flex gap-5 justify-between max-md:hidden">
-        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/f69d4ffe1982e44a988d673c998cb49f8a46ea6c9d9bd2f57d76ff719699a08c?apiKey=8b5c844c9e604efba227618c0cf9b444&" alt="" className="shrink-0 w-5 aspect-square" />
+        <form>
+        {isSearchOpen && 
+          <input type="text" placeholder="Search" className="border-2 border-black rounded-lg p-1" />
+        }
+        </form>
+        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/f69d4ffe1982e44a988d673c998cb49f8a46ea6c9d9bd2f57d76ff719699a08c?apiKey=8b5c844c9e604efba227618c0cf9b444&" alt="" className="shrink-0 w-5 aspect-square cursor-pointer" onClick={() => setIsSearchOpen(!isSearchOpen)} />
         <Link to="/cart" onClick={() => setActiveLink('/Cart')}>
           <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/1e2f479a15be02d05dc0b55f501a147fb34b583f9ff035e94ef773eb2922f563?apiKey=8b5c844c9e604efba227618c0cf9b444&" alt="" className="shrink-0 w-6 aspect-square" data-testid="shopping-cart-icon"/>
         </Link>
-        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/97e04eece18ef753b843c79800bfca7ebd4a798b8b9c4f596fe361bf1a14ff56?apiKey=8b5c844c9e604efba227618c0cf9b444&" alt="" className="shrink-0 w-6 aspect-square" />
+        <Link to="/profile" onClick={() => setActiveLink('/Profile')}>
+          <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/97e04eece18ef753b843c79800bfca7ebd4a798b8b9c4f596fe361bf1a14ff56?apiKey=8b5c844c9e604efba227618c0cf9b444&" alt="" className="shrink-0 w-6 aspect-square" />
+        </Link>
       </div>
     </header>
   );
