0a56a59b20cd464571e8bf0fc1b42d0337347bca
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-13 19:01:26 +0800

    refactor: Update main.jsx to remove unused imports and simplify rendering logic

--- plantify/src/main.jsx
+++ plantify/src/main.jsx
@@ -1,38 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-import App from './App.jsx'
-import Shop from './components/Shop.jsx'
-import Product_info from './components/Product_info.jsx'
-import Cart from './components/Cart.jsx'
-import { CartProvider } from './components/CartContext.jsx' // Import CartProvider
-
-import './index.css'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/shop/:product_display',
-    element: <Shop />,
-  },
-  {
-    path: '/shop/:product_display/:product_name',
-    element: <Product_info />,
-  },
-  {
-    path: '/cart',
-    element: <Cart />,
-  }
-])
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CartProvider> {/* Wrap RouterProvider with CartProvider */}
-      <RouterProvider router={router}/>
-    </CartProvider>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
